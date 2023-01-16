8a7a3229b54758bec8adc1515001610593add33f
kaasikodes <thisbroisfresh@gmail.com>
2023-01-16 23:57:38 +0100

    wrote guideline 4 testing

--- public/tests.js
+++ public/tests.js
@@ -9,6 +9,17 @@
 // 2.) Third Party Code
 // 3.) Code that isnt important from a user a point of view
 
+// Priority Order 4 Queries
+// "YOUR TESTS SHOULD RESEMBLE HOW USERS INTERACT WITH YOUR CODE (COMPONENT, PAGE,ETC.) AS MUCH AS POSSIBLE"
+// 1.) GetbyRole (go thru docs to learn what role to use for each particular element)
+// 2.) GetByLabelText
+// 3.) GetByPlaceHolderText
+// 4.) GetByText
+// 5.) GetByDisplayValue
+// IF NONE OF ABOVE WORK use => GetByAltText, GetByTitle (varies from browser to browser, and is not visible by default to sight readers), GetByTestId (the user cannot see/hear this e.g text is dynamic)
 // TO DO
 // write tests
 // consume api endpoint & test it
+// Research more to learn what to test for
+// Learn a bit of cypress but not too much
+// go thru docs to learn what role to use for each particular element
