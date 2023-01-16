678753cb74d8e9083d0dface1228d37e40bc17cb
kaasikodes <thisbroisfresh@gmail.com>
2023-01-16 23:47:32 +0100

    wrote guideline 4 testing

--- public/tests.js
+++ public/tests.js
@@ -1,1 +1,14 @@
-// does the
+// What to test -> Starting Point
+// 1.) Test that component renders
+// 2.) Test that comp renders with props
+// 3.) Test that comp renders in diff states e.g loggin/logged out -> showing of navbar etc.
+// 4.) Test Comp reacts to events
+
+// What not to test -> Implementation Details
+// 1.) Implement Details -> should test the behavior of app & not how its implemented
+// 2.) Third Party Code
+// 3.) Code that isnt important from a user a point of view
+
+// TO DO
+// write tests
+// consume api endpoint & test it
