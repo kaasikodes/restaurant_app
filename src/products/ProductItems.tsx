3536f71456aeb071c5d74b50b9116bdb0d2e23fa
kaasikodes <thisbroisfresh@gmail.com>
2023-01-29 22:12:24 +0100

    made correction on productItems comp

--- src/products/ProductItems.tsx
+++ src/products/ProductItems.tsx
@@ -9,7 +9,11 @@ const ProductItems = ({ products }: IProps) => {
   return (
     <div className="grid gap-4 md:grid-cols-4">
       {products.map((item) => (
-        <ProductItem key={item.id} product={item} />
+        <ProductItem
+          key={item.id}
+          product={item}
+          handleClick={() => console.log(item.id)}
+        />
       ))}
     </div>
   );
